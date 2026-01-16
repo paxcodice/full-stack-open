2973155e682485b891cdcc706e61012f878e6964
paxcodice <paxcodice@proton.me>
2026-01-16 11:19:35 -0500

    Fixed button fuckery

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -18,12 +18,12 @@ const App = () => {
 		<div>
 			<h1>give feedback</h1>
 			<Button onClick={() => setGood(good + 1)} text={'good'} />
-			<Button onClick={() => setNeutral(bad + 1)} text={'neutral'} />
-			<Button onClick={() => setBad(neutral + 1)} text={'bad'} />
+			<Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
+			<Button onClick={() => setBad(bad + 1)} text={'bad'} />
 			<h1>statistics</h1>
-			{good}
-			{neutral}
-			{bad}
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
 		</div>
 	);
 };
