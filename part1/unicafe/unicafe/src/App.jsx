5065e8eb8acde9d5a6ea639e75940309456346ee
paxcodice <paxcodice@proton.me>
2026-01-16 15:11:39 -0500

    Change {props.total} to {props.all}

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -11,7 +11,7 @@ const Statistics = (props) => {
 				<StatisticLine text="good" value={props.good} />
 				<StatisticLine text="neutral" value={props.neutral} />
 				<StatisticLine text="bad" value={props.bad} />
-				<StatisticLine text="all" value={props.total} />
+				<StatisticLine text="all" value={props.all} />
 				<StatisticLine text="average" value={props.average} />
 				<StatisticLine text="positive" value={props.positive} unit="%" />
 			</tbody>
