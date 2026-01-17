8560f7ce6a7dd97e9881fe098d61d8f6d22cd75a
paxcodice <paxcodice@proton.me>
2026-01-17 11:29:35 -0500

    2.5: Separate module step 10 COMPLETE

--- part2/courseinfo/courseinfo/src/App.jsx
+++ part2/courseinfo/courseinfo/src/App.jsx
@@ -1,38 +1,5 @@
-const Header = (props) => <h1>{props.course}</h1>;
+import Course from './components/Course';
 
-const Content = (props) => (
-  <div>
-    {props.parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-// I instinctively used reduce in the previous exercise lol
-const Total = (props) => {
-  const sum = props.parts.reduce((acc, cur) => acc + cur.exercises, 0);
-  return (
-    <p>
-      <strong>total of {sum} exercises</strong>
-    </p>
-  );
-};
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
-    </div>
-  );
-};
 const App = () => {
   const courses = [
     {
@@ -79,9 +46,14 @@ const App = () => {
     },
   ];
 
-  return courses.map((course) => {
-    return <Course key={course.id} course={course} />;
-  });
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => {
+        return <Course key={course.id} course={course} />;
+      })}
+    </div>
+  );
 };
 
 export default App;
---
+++ part2/courseinfo/courseinfo/src/components/Course.jsx
@@ -0,0 +1,37 @@
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+// I instinctively used reduce in the previous exercise lol
+const Total = (props) => {
+  const sum = props.parts.reduce((acc, cur) => acc + cur.exercises, 0);
+  return (
+    <p>
+      <strong>total of {sum} exercises</strong>
+    </p>
+  );
+};
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  );
+};
+
+export default Course;
