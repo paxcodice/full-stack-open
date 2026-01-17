fceaa3aa157afffc9d34a0627828088645dc08f4
paxcodice <paxcodice@proton.me>
2026-01-17 16:54:02 -0500

    2.8: The Phonebook Step 3 COMPLETE

---
+++ part2/phonebook/phonebook/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name:{' '}
+          <input value={newName} onChange={handlePersonChange} placeholder="" />
+        </div>
+
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+
+        <div onSubmit={addPerson}>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => {
+          return (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
